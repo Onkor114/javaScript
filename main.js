cb15d01788dfe5589776642dc88ba1e745af21b9
Onkor114 <abdinourahmed114@gmail.com>
2024-08-03 17:49:46 +0300

    updated js file

--- main.js
+++ main.js
@@ -1,5 +1,52 @@
-var city = 'jigjiga';
+// var city = 'jigjiga';
 
-let name = 'Sonkor'
-const age = '50'
-console.log(age);
+// let name = 'Sonkor'
+// const age = '50'
+// console.log(age);
+
+// let greeting = "asalamu alaikum";
+// console.log(greeting);
+
+// let year= 2024;
+// console.log(year);
+
+// let isAdult =true;
+// console.log(isAdult);
+
+// let A = 10;
+// let B = 10;
+
+// console.log(A+B);
+// console.log(A-B);
+// console.log(A*B);
+// console.log(A/B);
+
+
+
+// console.log(A%B);
+// console.log(A++);
+// console.log(A);
+// console.log(A--);
+// console.log(++A);
+// console.log(--A);
+// console.log(--B);
+
+
+// let x = 7;
+// let y = 3;
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(++x);
+// console.log(++y);
+// console.log(x++);
+// console.log(y--);
+
+
+
+
+
+
