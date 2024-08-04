19b6234d5f4c3b9ca8b4d4c1236d37d348769bf5
Onkor114 <abdinourahmed114@gmail.com>
2024-08-04 12:42:09 +0300

    added functions

--- main.js
+++ main.js
@@ -46,6 +46,82 @@
 // console.log(y--);
 
 
+// let A = 15;
+
+// let B = "15";
+
+// console.log(A==B);
+// console.log(A!=B);
+// console.log(A===B);
+// console.log(A!==B);
+// console.log(A>B);
+// console.log(A>=B);
+// console.log(A<B);
+// console.log(A<=B);
+
+
+// LOGICAL OPERATORS
+
+// let isSunny = true;
+
+// let isWeekend = false;
+
+// console.log(isSunny && isWeekend);
+
+// console.log(isSunny|| isWeekend);
+// console.log(!isSunny);
+// console.log(!isWeekend)
+
+// String OPERATORS
+
+// let firstName = " Abdinour";
+// let lastName = " muktar";
+
+// // console.log(firstName +  lastName);
+// // console.log(lastName);
+
+// let greeting = "Hello";
+
+// greeting += firstName;
+// greeting += lastName;
+
+// greeting += " good morning";
+
+// console.log(greeting);
+
+
+
+//##Function declaration
+
+// function great(name){
+//   console.log("Hello " + name);
+
+// }
+// great("Abdinour");
+// great("Sonkor");
+// great("Abd");
+
+
+// function calculateTotal(price,quantity){
+//   console.log("Total price:  $" +price * quantity);
+// }
+// calculateTotal(2,10);
+// calculateTotal(5,50);
+// calculateTotal(3,6);
+
+//#RETURN FUNCTION
+function add(a,b){
+  // console.log(a+b);
+  return a+b;
+}
+// console.log(add(10,30));
+
+let result = add(10,30)
+console.log(result /2);
+console.log(result * 2)
+
+
+
 
 
 
